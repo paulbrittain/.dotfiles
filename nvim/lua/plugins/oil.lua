252af03d4e31b81732e3dee07cd620205691fd78
Paul Brittain <paulsabana@gmail.com>
2024-12-18 07:55:22 +0100

    use default oil icons

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -6,6 +6,6 @@ return {
         vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Oil Open parent directory" })
       },
       -- Optional dependencies
-      --dependencies = { { "echasnovski/mini.icons", opts = {} } },
-       dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+      dependencies = { { "echasnovski/mini.icons", opts = {} } },
+       -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
