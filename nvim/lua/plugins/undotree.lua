ba48562bd7409e11f958d8e1387cb07822d91db0
Paul Brittain <paulsabana@gmail.com>
2025-02-17 10:50:06 +0100

    lazy loading undotree stopped the keybind from working

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -1,7 +1,7 @@
 return {
     {
       "mbbill/undotree",
-      lazy = true,
+      lazy = false,
       config = function()
         -- Key mapping for toggling undotree
         vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { noremap = true, silent = true })
