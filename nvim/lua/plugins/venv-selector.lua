9b839285a2f2f3e987a6b62586904fdfee641399
Paul Brittain <paulsabana@gmail.com>
2025-06-01 13:53:09 +0200

    improve venv-selector config
    - add descriptions for bindings

--- nvim/lua/plugins/venv-selector.lua
+++ nvim/lua/plugins/venv-selector.lua
@@ -14,8 +14,8 @@ return {
     -- event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
     keys = {
         -- Keymap to open VenvSelector to pick a venv.
-        { '<leader>vs', '<cmd>VenvSelect<cr>' },
+        { '<leader>vs', '<cmd>VenvSelect<cr>', desc = "VenvSelector Open" },
         -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-        { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+        { '<leader>vc', '<cmd>VenvSelectCached<cr>', desc = "VenvSelector GetFromCache" },
     },
 }
