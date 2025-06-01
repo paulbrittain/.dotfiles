6f133cc388356c7b99c044f77dcbd93fd421e4a1
Paul Brittain <paulsabana@gmail.com>
2025-06-01 13:52:46 +0200

    improve undotree config
    - move keybind to keys obj
    - add description

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -2,9 +2,13 @@ return {
     {
         "mbbill/undotree",
         lazy = false,
-        config = function()
-            -- Key mapping for toggling undotree
-            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { noremap = true, silent = true })
-        end,
+        keys = {
+            {
+                "<leader>u",
+                function() vim.cmd.UndotreeToggle() end,
+                desc = "UndotreeToggle",
+                mode = "n",
+            },
+        }
     }
 }
