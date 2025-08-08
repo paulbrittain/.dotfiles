2f8e86db1b011c92fe08bb218b64987cee78230f
Paul Brittain <paulsabana@gmail.com>
2025-08-08 09:27:22 +0200

    update treesitter formatter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -27,6 +27,7 @@ return {
                 pattern = filetypes,
                 callback = function()
                   vim.treesitter.start()
+                  vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
                  end,
               })
             end
